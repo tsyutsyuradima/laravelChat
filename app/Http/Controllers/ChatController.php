f7697be03a2d5b794de3eb4391d6da82f62d956a
tsyutsyuradima <Lord!!1993>
2015-03-18 11:36:45 +0200

    Test commit

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -1,6 +1,11 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Database;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+
 class ChatController extends Controller {
 
     public function __construct()
@@ -9,6 +14,36 @@ class ChatController extends Controller {
 
     public function index()
     {
-        return view('chat');
+        if ($_POST) {
+            echo json_encode(['result'=>'success']);
+        }
+        else {
+            return view('chat');
+        }
     }
+
+    public function login()
+    {
+    }
+
+    public function create() {
+//        //check if its our form
+//        if ( Session::token() !== Input::get( '_token' ) ) {
+//            return Response::json( array(
+//                'msg' => 'Unauthorized attempt to create setting'
+//            ) );
+//        }
+//
+//        $setting_name = Input::get( 'setting_name' );
+//        $setting_value = Input::get( 'setting_value' );
+//
+//
+        $response = array(
+            'status' => 'success',
+            'msg' => 'Setting created successfully',
+        );
+
+        return Response::json( $response );
+    }
+
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,10 @@
 <html>
+<head>
+    <link type="text/css" rel="stylesheet" href="/css/main.css" />
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans Condensed:300italic,300,700" rel="stylesheet" type="text/css">
+</head>
 <body>
-<h1>Laravel Quickstart</h1>
+<h1 style="text-align: center">Laravel Quick chat</h1>
 
 @yield('content')
 </body>
